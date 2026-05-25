8dc58b77091cf621dcf5a2def13c908e813ff3f1
Otinpan <hasesyou7531@gmail.com>
2026-05-25 22:16:17 +0900

    remove unrelated folders

--- atcoder/AHC062/build/CMakeFiles/ahc062.dir/src/map.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ahc062.dir/src/map.cpp.o: \
- /home/hase/trace-pilot-experiment/atcoder/AHC062/src/map.cpp \
- /usr/include/stdc-predef.h
--- atcoder/AHC063/build/CMakeFiles/submit_cpp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/submit_cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/submit_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
